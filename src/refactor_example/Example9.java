4b22b2a9f64bb02a2d01a66b35f0301b8c689787
MaximLuninn <zekamarkin2001@mail.ru>
2024-06-14 14:15:30 +0300

    Lab_13

---
+++ src/refactor_example/Example9.java
@@ -0,0 +1,14 @@
+package refactor_example;
+public class Example9 {
+    public static int ma() {
+        try {
+            System.out.println("0");
+            return 55;    // выход из метода
+        } finally {
+            System.out.println("1");
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(ma());
+    }
+}
